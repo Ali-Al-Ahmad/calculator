e833897f33289490865f9c42a2f1fdbd00722e75
Ali-Al-Ahmad <ali.alahmad.cs@gmail.com>
2024-12-20 08:32:57 +0200

    implemented calculatePrefixOrPostfix + isOperator function

--- scripts/app.js
+++ scripts/app.js
@@ -104,3 +104,82 @@ function removeRows() {
   document.querySelector('#stack-table tbody').innerHTML = ''
 }
 
+function calculatePrefixOrPostfix() {
+  expression = expression.trim()
+  if (isNotValidExpression()) {
+    expression = 'Invalid Expression'
+    return updateResult()
+  }
+
+  const temp_array = formatSpacesInExpression(expression).split(' ')
+
+  let isPrefix = isOperator(temp_array[0])
+
+  if (!isPrefix) {
+    temp_array.reverse()
+  }
+  let my_stack = []
+  for (let i = temp_array.length - 1; i >= 0; i--) {
+    let current_expression = temp_array[i]
+    let num1, num2
+    if (!isOperator(current_expression)) {
+      my_stack.push(parseFloat(current_expression))
+    } else {
+      if (my_stack.length < 2) {
+        expression = 'Invalid Expression'
+        return updateResult()
+      }
+
+      num1 = my_stack[my_stack.length - 1]
+      my_stack.pop()
+      num2 = my_stack[my_stack.length - 1]
+      my_stack.pop()
+
+      if (current_expression == '+') {
+        my_stack.push(num1 + num2)
+      }
+      if (current_expression == '-') {
+        my_stack.push(isPrefix ? num1 - num2 : num2 - num1)
+      }
+      if (current_expression == '*' || current_expression == 'x') {
+        my_stack.push(isPrefix ? num1 * num2 : num2 * num1)
+      }
+      if (current_expression == '/') {
+        my_stack.push(isPrefix ? num1 / num2 : num2 / num1)
+      }
+    }
+
+    let table_stack_expression = current_expression
+    if (isOperator(current_expression) && isPrefix) {
+      table_stack_expression = `(${num1} ${current_expression} ${num2})`
+    } else if (isOperator(current_expression) && !isPrefix) {
+      table_stack_expression = `(${num2} ${current_expression} ${num1})`
+    }
+
+    createTableRow(
+      -(i - temp_array.length),
+      current_expression,
+      table_stack_expression
+    )
+  }
+
+  if (my_stack.length > 1) {
+    expression = 'Invalid Expression'
+    return updateResult()
+  }
+  let last_result = my_stack[my_stack.length - 1]
+  createTableRow('Result', expression, last_result.toFixed(2))
+  expression = last_result.toFixed(2)
+
+  return updateResult()
+}
+
+function isOperator(number) {
+  return (
+    number == '+' ||
+    number == '-' ||
+    number == 'x' ||
+    number == '*' ||
+    number == '/'
+  )
+}
