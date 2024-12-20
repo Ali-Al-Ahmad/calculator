3ceda5e1265416814a13d13cb4734f7cc078cde9
Ali-Al-Ahmad <ali.alahmad.cs@gmail.com>
2024-12-20 08:22:37 +0200

    declared variable for HTML elements(buttons + input + operants)

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+let expression = ''
+
+const result = document.getElementById('result')
+const numbers = document.querySelectorAll('.num-op')
+const clear_all_ac = document.getElementById('clear-All')
+const delete_last = document.getElementById('delete-last')
+const equal_button = document.getElementById('equal-btn')
+const space_button = document.getElementById('space-btn')
+const button_dark_mode = document.getElementById('dark-mode')
+
