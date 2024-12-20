56f83c3bf74bf4185f450b1fdb878a64c930a9b5
Ali-Al-Ahmad <ali.alahmad.cs@gmail.com>
2024-12-20 08:25:29 +0200

    added event listeners to all buttons

--- scripts/app.js
+++ scripts/app.js
@@ -8,3 +8,30 @@ const equal_button = document.getElementById('equal-btn')
 const space_button = document.getElementById('space-btn')
 const button_dark_mode = document.getElementById('dark-mode')
 
+result.addEventListener('input', function (event) {
+  expression = event.target.value
+})
+result.addEventListener('keydown', function (event) {
+  const allowedKeys = ['Backspace','0','1','2','3','4','5','6','7','8','9','+','-','*','/','x']
+
+  if (!allowedKeys.includes(event.key)) {
+    event.preventDefault()
+  }
+})
+numbers.forEach((element) => {
+  element.addEventListener('click', digitClick)
+})
+delete_last.addEventListener('click', deleteLastCharacter)
+clear_all_ac.addEventListener('click', clearAllFucntion)
+equal_button.addEventListener('click', calculatePrefixOrPostfix)
+button_dark_mode.addEventListener('click', changeToDarkMode)
+
+space_button.addEventListener('click', addSpace)
+document.addEventListener('keydown', function(event) {
+  if (event.code === 'Space') {
+    event.preventDefault()
+    addSpace()
+  }
+})
+
+
